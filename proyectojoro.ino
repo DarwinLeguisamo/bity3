8b362f9640a8e837ee6a35290429aefef405f073
DarwinLeguisamo <128190699+DarwinLeguisamo@users.noreply.github.com>
2025-08-29 13:50:38 -0300

    Update proyectojoro.ino

--- proyectojoro.ino
+++ proyectojoro.ino
@@ -13,85 +13,69 @@ int N3=930;
 int N4=930;
 int N5=930;
 void setup() {
-  pinMode(4, OUTPUT); // El pin Trig como salida
-  pinMode(7, INPUT);
-  pinMode(5, OUTPUT); // El pin Trig como salida
-  pinMode(6, INPUT);
+  pinMode(4, OUTPUT); // El pin Trig como salida adelnate
+  pinMode(7, INPUT);// Echco adelante
+  pinMode(5, OUTPUT); // El pin Trig como salida costado
+  pinMode(6, INPUT);// echo del costado
   Serial.begin(9600);
-    brazo.attach(8);
-    
-    pinza.attach(2);
-    mano.attach(9); // indico donde esta mi servo conectado 
- 
-   brazo.write(90);
-    mano.write(90);
- pinza.write(90);
- delay(500);
- aumentar_angulo(140,pinza,100);
+  brazo.attach(8);
+  pinza.attach(2);
+  mano.attach(9); // indico donde esta mi servo conectado 
+  brazo.write(90);
+  mano.write(90);
+  pinza.write(90);
+  delay(500);
+  aumentar_angulo(140,pinza,100);
   pinMode(DirA,OUTPUT);
   pinMode(DirB,OUTPUT);
   pinMode(PotA,OUTPUT);
   pinMode(PotB,OUTPUT);
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   while (calibrar){
     Serial.print("A4..");
     Serial.println(analogRead(A4));
-
-       Serial.print("A4..");
+    Serial.print("A4..");
     Serial.println(analogRead(A4));
-
-
-       Serial.print("A4..");
+    Serial.print("A4..");
     Serial.println(analogRead(A4));
-
-
-       Serial.print("A4..");
+    Serial.print("A4..");
     Serial.println(analogRead(A4));
-
     delay(300);    
     }
   while(!calibrar){  // programa principal 
-
-   if (distancia(4,7)<10){
-    adelante(0,0);
-    delay(1000);
-    agarrar();
-    pelota=true;
-    adelante(50,50);
+    if (distancia(4,7)<10){
+      adelante(0,0);
+      delay(1000);
+      agarrar();
+      pelota=true;
+      adelante(50,50);
     }
-   else{
-    if(analogRead(A3)>N3){
-      adelante(100,50);
-      }
-     if(analogRead(A2)>N2){
-      derecha(130,130);
-     }
+    else{
+      if(analogRead(A3)>N3){
+        adelante(100,50);
+        }
+      if(analogRead(A2)>N2){
+        derecha(130,130);
+       }
       if(analogRead(A4)>N4){
-      adelante(50,100);
+        adelante(50,100);
      }
       if(analogRead(A5)>N5){
-        
-      izquierda(150,150);
+        izquierda(150,150);
       }
 
-    if ((!(analogRead(A3)>N3))&&(!(analogRead(A2)>N2))&&(!(analogRead(A4)>N4))&&(!(analogRead(A5)>N5))){
-      adelante(50,50);}
-   }
-   
+      if ((!(analogRead(A3)>N3))&&(!(analogRead(A2)>N2))&&(!(analogRead(A4)>N4))&&(!(analogRead(A5)>N5))){
+        adelante(50,50);}
+    }
     if ((distancia(5,6)<20) && (pelota)){
       adelante(0,0);
       delay(1000);
-      while(distancia(4,7)<10);
-        izquierda(150,150);
-        
-  {
-      }
-   
-  }  
+      while(distancia(4,7)<10){
+        izquierda(150,150);}
+    }  
  
 
 }
@@ -173,3 +157,4 @@ void agarrar(){
     disminuir_angulo(10,mano,50);
    // aumentar_angulo(140,pinza,100);
   }
+
